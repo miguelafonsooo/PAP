3cef109f6b8b2f52d5ffaa91325ddc144771482a
miguelafonsooo <151622232+miguelafonsooo@users.noreply.github.com>
2024-01-16 20:48:26 +0000

    atualizei random

--- php/registrocartao.php
+++ php/registrocartao.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>Registar Cartão</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 </head>
