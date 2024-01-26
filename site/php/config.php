efa6935b920be8f519e02d12ab55c054024f7cfc
miguelafonsooo <151622232+miguelafonsooo@users.noreply.github.com>
2024-01-26 15:53:35 +0000

    update no codigo node red

--- site/php/config.php
+++ site/php/config.php
@@ -1,7 +1,6 @@
 <?php
     $con = mysqli_connect("localhost", "root", "", "pap-maquina-de-vendas");
 
-    // Verificar a ligação
     if (!$con) {
         die("Erro na ligação: " . mysqli_connect_error());
     }
