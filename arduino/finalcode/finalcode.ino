d3c695a0aa93181d401a834006bba146865fe889
miguelafonsooo <151622232+miguelafonsooo@users.noreply.github.com>
2024-02-23 19:07:45 +0000

    atualizei o code do node-red

--- arduino/finalcode/finalcode.ino
+++ arduino/finalcode/finalcode.ino
@@ -1,6 +1,8 @@
 #include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define SS_PIN 53       // Pino para SDA (SS) no Arduino Mega
 #define RST_PIN 8
@@ -14,9 +16,9 @@ Servo servo2;           // Objeto do Servo 2
 Servo servo3;           // Objeto do Servo 3
 Servo servo4;           // Objeto do Servo 4
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
   pinMode(BUTTON_PIN_1, INPUT_PULLUP);
@@ -27,9 +29,17 @@ void setup() {
   servo2.attach(10);    // Conecta o servo 2 ao pino digital 10
   servo3.attach(11);    // Conecta o servo 3 ao pino digital 11
   servo4.attach(12);    // Conecta o servo 4 ao pino digital 12
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  Serial.begin(9600);
+  lcd.print("Passe o cartao.");
 }
 
 void loop() {
+
+
   // Lógica RFID
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
     String cardUID = "";
@@ -54,6 +64,9 @@ void loop() {
       moveServo(servo4, 180);
     } else if (input.equals("finalizar")) {
       stopServos();
+    } else if (input.startsWith("g")) {
+      String message = input.substring(1); // Remove o caractere de controle "g"
+      displayMessage(message);
     }
   }
 
@@ -71,8 +84,9 @@ void loop() {
     Serial.println("E");
     delay(1000);
   }
-}
 
+ 
+}
 void moveServo(Servo servo, int angle) {
   servo.write(angle);
   delay(1000);
@@ -83,4 +97,23 @@ void stopServos() {
   servo2.write(90);
   servo3.write(90);
   servo4.write(90);
+}
+
+void displayMessage(String message) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  
+  // Imprime a primeira linha
+  lcd.print(message.substring(0, 16));
+  
+  // Verifica se há mais texto para exibir
+  if (message.length() > 16) {
+    // Calcula o número de caracteres restantes
+    int remainingChars = message.length() - 16;
+    // Imprime o texto restante em linhas adicionais
+    for (int i = 0; i < remainingChars; i += 16) {
+      lcd.setCursor(0, 1); // Muda para a segunda linha
+      lcd.print(message.substring(16 + i, min(16 + i + 16, message.length())));
+    }
+  }
 }
